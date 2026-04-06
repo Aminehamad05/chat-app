fa5bc584834696b7bb0516f1dc5c85b5200f6e3b
Aminehamad05 <amine.hamed@insat.ucar.tn>
2026-04-07 00:16:13 +0100

    backend done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
+import cookiePaser from "cookie-parser"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
