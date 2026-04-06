ec456929d4e5a5184d0230ed38ad05809f7c766e
Aminehamad05 <aminecrck0000@gmail.com>
2026-04-06 14:36:02 +0100

    signup done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 export const signup = async (req,res)=>{
@@ -12,8 +13,27 @@ export const signup = async (req,res)=>{
         if (user) return req.status(400).json({message : "Email already exists"})
         const salt = await bcrypt.genSalt(10)
         const hashedPassword =await bcrypt.hash(password,hash)
+        const newUser= new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+        if (newUser){
+            generateToken(newUser._id,res)
+            await newUser.save()
+            res.status(201).json({
+                _id:newUser._id,
+                fullName : newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+
+            })
+        }else{
+            res.status(400).json({message:"Invalid user data"})
+        }
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({message:"Internal Server Message"})
     }
 }
 export const login = (req,res)=>{
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,10 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json())
+app.use(express.json({limit:'10mb'}))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use("/api/auth",authRoutes)
 app.listen(PORT, () => {
     console.log(`SERVER running  on ${PORT}`);
