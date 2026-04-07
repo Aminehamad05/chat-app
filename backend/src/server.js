e0f6c58d743d4e424a941e7e1e5893ce36fe3cae
Aminehamad05 <amine.hamed@insat.ucar.tn>
2026-04-07 13:46:31 +0100

    message route done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
 import { connectDB } from './lib/db.js';
 import cookiePaser from "cookie-parser"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json({limit:'10mb'}))
+app.use(cookiePaser())
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes);
 app.listen(PORT, () => {
     console.log(`SERVER running  on ${PORT}`);
     connectDB()
